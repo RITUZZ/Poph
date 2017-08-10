d26ab3cb86becf275d1c9bfb273a17fd56446e58
Graduate <Graduate>
2017-08-10 12:05:05 +0100

    databases actually split

--- src/main/java/DatabaseTier/DatabaseManagerOriginal.java
+++ src/main/java/DatabaseTier/DatabaseManagerOriginal.java
@@ -1,33 +1,28 @@
 package DatabaseTier;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Properties;
 
-import BusinessTier.HashMaster;
-import model.User;
+public class DatabaseManagerOriginal {
 
-public class DatabaseConnector {
-
-	private Connection connection;
 	static Properties configProps;
+	private Connection connection;
 
 	public static void main(String[] args) {
-		DatabaseConnector d = new DatabaseConnector();
+		DatabaseManagerOriginal d = new DatabaseManagerOriginal();
 		ArrayList<String> s = d.getTables();
 		for (String sd : s) {
 			System.out.println(sd);
 		}
 	}
 
-	public DatabaseConnector() {
+	public DatabaseManagerOriginal() {
 
 		try {
 
@@ -69,17 +64,18 @@ public class DatabaseConnector {
 
 	}
 
-	public User checklogin(String username) {
-		try {
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM users WHERE user_id = ?");
-			ps.setString(1, username);
-			ResultSet rs = ps.executeQuery();
+	public ArrayList<String> getTables() {
 
-			if (rs.next()) {
-				return new User(username, rs.getString("user_pwd"), rs.getString("user_salt"));
-			} else {
-				System.err.println("Username not found");
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery("SHOW TABLES");
+			ArrayList<String> tables = new ArrayList<String>();
+
+			while (rs.next()) {
+				rs.getString(1);
 			}
+
+			return tables;
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -87,28 +83,4 @@ public class DatabaseConnector {
 		return null;
 	}
 
-
-	public boolean registerUser(String user, String password) {
-		try {
-			PreparedStatement ps = connection.prepareStatement("INSERT INTO userSecure (user_id, user_pwd) VALUES (?, ?)");
-			ps.setString(1, user);
-			ps.setString(2, password);
-			ResultSet rs = ps.executeQuery();
-
-			while (rs.next()) {
-				String userResult = rs.getString("user_id");
-				if (userResult != null && !userResult.equals("")) {
-					return true;
-				}
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	
-
 }
-
