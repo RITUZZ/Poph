22270fe3417328833b49fe0e4909e26b89975805
Graduate <Graduate>
2017-08-15 14:10:19 +0100

    separated instrument json logic

---
+++ src/main/java/BusinessTier/InstrumentTable.java
@@ -0,0 +1,84 @@
+package BusinessTier;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import DatabaseTier.DatabaseManagerOriginal;
+import model.Instrument;
+
+/**
+ * Servlet implementation class InstrumentTable
+ */
+public class InstrumentTable extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InstrumentTable() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void getInstrumentTable(DatabaseManagerOriginal db, ObjectMapper mapper, PrintWriter out) throws JsonGenerationException, JsonMappingException, IOException{
+    	ArrayList<Instrument> instrumentList = new ArrayList<Instrument>();
+		List<ObjectNode> answerList = new ArrayList<ObjectNode>();
+		instrumentList = db.getInstrumentTable();
+		Instrument instrument;
+		
+		for(int i=0; i<instrumentList.size();i++){
+			instrument = instrumentList.get(i);
+				ObjectNode node = JsonNodeFactory.instance.objectNode();
+				node.put("id",instrument.getId());
+				node.put("name", instrument.getName());
+				answerList.add(node);
+			
+		}
+		mapper.writeValueAsString(answerList);
+		ObjectNode responseNode = JsonNodeFactory.instance.objectNode();
+		
+		//hardcoded true for now
+		responseNode.put("status", true);
+		
+		ArrayNode array = mapper.valueToTree(answerList);
+		responseNode.put("answer", array);
+		String jsonInString = mapper.writeValueAsString(responseNode);
+		out.println(jsonInString);
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		DatabaseManagerOriginal db = new DatabaseManagerOriginal();
+		ObjectMapper mapper = new ObjectMapper();
+		PrintWriter out = response.getWriter();
+		
+		//calling method to get Instrument table
+		getInstrumentTable(db, mapper, out);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
