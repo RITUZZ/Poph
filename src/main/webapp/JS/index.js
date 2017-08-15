8be116d668eae9962d514ce100432a1233cc1508
Graduate <Graduate>
2017-08-15 14:25:13 +0100

    dashboard working

--- src/main/webapp/JS/index.js
+++ src/main/webapp/JS/index.js
@@ -11,6 +11,7 @@ app.controller('SubmitController',function($scope,$http,$rootScope){
 				};
 			var root = '';
 			$scope.View.url="HTML/dashboard.html";
+			/*
 			$http({
 			  url: root + 'Login',
 			  method: 'POST',
@@ -29,9 +30,28 @@ app.controller('SubmitController',function($scope,$http,$rootScope){
 			},function error(data){
 				console.log("error");
 			});
+			*/
 		}
 	});
 app.controller("main-controller",function($scope){
 	$scope.View={};
-	$scope.View.url="HTML/login.html ";
+	$scope.View.subUrl="HTML/dash-tradeoverview.html";
+	$scope.View.url="HTML/dashboard.html ";
+	
+	
+	$scope.navoptions=function(obj){
+		
+		var temp = angular.element(".dash-nav>.navbar-nav>li").removeClass('active');
+		var type = angular.element(obj.target.parentNode).attr('navtype');
+		if(type=='1')
+			{
+				$scope.View.subUrl="HTML/dash-tradeoverview.html";
+			}
+		else if(type=='2')
+			{
+				$scope.View.subUrl="HTML/dash-tableview.html";
+			}
+		 $(obj.target.parentNode).addClass('active');
+	};
+	
 });
